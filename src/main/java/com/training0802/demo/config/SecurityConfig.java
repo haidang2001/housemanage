5103aaa9915a1aae6d373c67b352663370b9742e
haidang2001 <nchdang16012001@gmail.com>
2024-03-16 17:42:31 +0700

    delete preauthorize

--- src/main/java/com/training0802/demo/config/SecurityConfig.java
+++ src/main/java/com/training0802/demo/config/SecurityConfig.java
@@ -12,9 +12,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
-@EnableMethodSecurity
+//@Configuration
+//@EnableWebSecurity
+//@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
