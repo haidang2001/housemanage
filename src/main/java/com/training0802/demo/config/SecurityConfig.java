067dbd357a74315c9f2032b4ef97d5104b6340e5
haidang2001 <nchdang16012001@gmail.com>
2024-03-16 17:37:34 +0700

    delete preauthorize

--- src/main/java/com/training0802/demo/config/SecurityConfig.java
+++ src/main/java/com/training0802/demo/config/SecurityConfig.java
@@ -1,22 +1,22 @@
-//package com.training0802.demo.config;
-//
-//import com.training0802.demo.service.mysql.AccountInfoToUserDetailsService;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//@EnableMethodSecurity
-//public class SecurityConfig {
-//
+package com.training0802.demo.config;
+
+import com.training0802.demo.service.mysql.AccountInfoToUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
 //    @Bean
 //    public PasswordEncoder passwordEncoder() {
 //        return new BCryptPasswordEncoder();
@@ -27,21 +27,21 @@
 //    public UserDetailsService userDetailsService() {
 //        return new AccountInfoToUserDetailsService();
 //    }
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf().disable()
+//                .cors().and()
+//                .authorizeHttpRequests((authr) -> authr
+//                        .requestMatchers("/api/acc/add").permitAll()
+//                        .requestMatchers("/api/acc/login").permitAll()
+////                        .requestMatchers("/api/account/add").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .httpBasic(Customizer.withDefaults());
 //
-////    @Bean
-////    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-////        http
-////                .csrf().disable()
-////                .cors().and()
-////                .authorizeHttpRequests((authr) -> authr
-////                        .requestMatchers("/api/acc/add").permitAll()
-////                        .requestMatchers("/api/acc/login").permitAll()
-//////                        .requestMatchers("/api/account/add").permitAll()
-////                        .anyRequest().authenticated()
-////                )
-////                .httpBasic(Customizer.withDefaults());
-////
-////        return http.build();
-////    }
-//}
-//
+//        return http.build();
+//    }
+}
+
